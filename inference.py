2b0d54e427f9126e9b5a3f83c159f5c02a64401b
arturo <arturo.gil@umh.es>
2025-10-15 17:48:42 +0200

    First commit

--- inference.py
+++ inference.py
@@ -1,18 +1,21 @@
 # This is a sample Python script.
 # pip install ultralytics
 # !git clone https://github.com/alsombra/Mask_RCNN-TF2
-import cv2
+# import cv2
 from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('yolov8n.pt')  # tiny model
-    # model.train(data='trash.yaml', epochs=50, imgsz=640)
-    # model = YOLO('runs/detect/train/weights/best.pt')
-    cv2.waitKey(delay=5)
-    # image_path= '../TACO/data/batch_1/000000.jpg'
+    # Select
+    # tiny default model and some test images
     # image_path = '../TACO/data/people/dog.jpg'
-    image_path = '../TACO/data/people/tennis.jpg'
+    # image_path = '../TACO/data/people/tennis.jpg'
+    # model = YOLO('yolov8n.pt')
+
+    # tiny model trained with TACO
+    image_path = 'datasets/taco/images/val/000000.jpg'
+    model = YOLO('runs/detect/train/weights/best.pt')
+
     results = model.predict(source=image_path, conf=0.5, show=True, save=True)
     print(results)
     for result in results:
@@ -20,4 +23,4 @@ if __name__ == '__main__':
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- split_dataset_stepB.py
+++ split_dataset_stepB.py
@@ -2,7 +2,17 @@ import os, random, shutil
 
 # Paths
 #input path
-img_labels_dirs = ["../TACO/data/batch_1", "../TACO/data/batch_2"]
+# add directories
+img_labels_dirs = ["../TACO/data/batch_1",
+                   "../TACO/data/batch_2",
+                   "../TACO/data/batch_8",
+                   "../TACO/data/batch_9",
+                   "../TACO/data/batch_10",
+                   "../TACO/data/batch_11",
+                   "../TACO/data/batch_12",
+                   "../TACO/data/batch_13",
+                   "../TACO/data/batch_14",
+                   "../TACO/data/batch_15"]
 # label_dir = "datasets/taco/labels_all"
 out_dir = "./datasets/taco"
 
@@ -16,6 +26,8 @@ os.makedirs(f"{out_dir}/labels/val", exist_ok=True)
 val_ratio = 0.2
 
 for img_labels_dir in img_labels_dirs:
+    # get the batch number. Som
+    batch_name = img_labels_dir.split('_')[1]
     # List all images
     images = [f for f in os.listdir(img_labels_dir) if f.endswith(".jpg")]
     random.shuffle(images)
@@ -29,10 +41,10 @@ for img_labels_dir in img_labels_dirs:
             subset = "val"
         else:
             subset = "train"
-
-        shutil.copy(f"{img_labels_dir}/{img_name}", f"{out_dir}/images/{subset}/{img_name}")
+        # finally, copy to directory with the batch name
+        shutil.copy(f"{img_labels_dir}/{img_name}", f"{out_dir}/images/{subset}/{batch_name+'_'+img_name}")
         # if os.path.exists(f"{label_dir}/{label_name}"):
-        shutil.copy(f"{img_labels_dir}/{label_name}", f"{out_dir}/labels/{subset}/{label_name}")
+        shutil.copy(f"{img_labels_dir}/{label_name}", f"{out_dir}/labels/{subset}/{batch_name+'_'+label_name}")
 
 
 
