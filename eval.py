f0770d5f587fd1b31d05bad9dfffeff139806115
Arturo <arturo.gil@umh.es>
2025-10-16 18:35:34 +0200

    second commit

--- eval.py
+++ eval.py
@@ -69,7 +69,7 @@ def yolo_trash_on_trash_video2():
     return image_path, model, delay
 
 
-def inference(image_path, model, delay, conf=0.5):
+def inference(image_path, model, delay, conf=0.5, title='Yolo trash detection'):
     # inference on every of the dataset.
     images = [f for f in os.listdir(image_path) if f.endswith(".jpg")]
     images = sorted(images)
@@ -79,7 +79,7 @@ def inference(image_path, model, delay, conf=0.5):
         # print(results)
         annotated_frame = results[0].plot()
         annotated_frame = cv2.resize(annotated_frame, display_size)
-        cv2.imshow('Yolo trash detection', annotated_frame)
+        cv2.imshow(title, annotated_frame)
         if cv2.waitKey(delay=delay) & 0xFF == ord('q'):
             break
         # for result in results:
@@ -91,22 +91,22 @@ if __name__ == '__main__':
     display_size = (1200, 1000)
 
     image_path, model, delay = yolo_orig_on_people()
-    inference(image_path=image_path, model=model, delay=delay)
+    inference(image_path=image_path, model=model, delay=delay, title='ORIG. YOLO ON PEOPLE')
 
     image_path, model, delay = yolo_trash_on_people()
-    inference(image_path=image_path, model=model, delay=delay)
+    inference(image_path=image_path, model=model, delay=delay, title='TRASH YOLO ON PEOPLE')
 
     image_path, model, delay = yolo_trash_on_trash_train()
-    inference(image_path=image_path, model=model,conf=0.4, delay=delay)
+    inference(image_path=image_path, model=model, conf=0.4, delay=delay, title='TRASH YOLO ON TRASH TRAIN')
 
     image_path, model, delay = yolo_trash_on_trash_val()
-    inference(image_path=image_path, model=model, conf=0.4, delay=delay)
+    inference(image_path=image_path, model=model, conf=0.4, delay=delay, title='TRASH YOLO ON TRASH EVAL')
 
     image_path, model, delay = yolo_trash_on_trash_video1()
-    inference(image_path=image_path, model=model, conf=0.4, delay=delay)
+    inference(image_path=image_path, model=model, conf=0.4, delay=delay, title='TRASH YOLO ON VIDEO 1')
 
     image_path, model, delay = yolo_trash_on_trash_video2()
-    inference(image_path=image_path, model=model, conf=0.2, delay=delay)
+    inference(image_path=image_path, model=model, conf=0.2, delay=delay, title='TRASH YOLO ON VIDEO 2')
 
 
 
