f15e967783aaf6b59dae3bf867028a6bd54b8f0e
Arturo <arturo.gil@umh.es>
2025-10-16 18:39:07 +0200

    second commit

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('yolov8n.pt')  # smallest model (fast)
     # train 5 best
-    # model.train(data='datasets/trash_taco.yaml', epochs=50, imgsz=640, freeze=0)
-    model.train(data='datasets/trash_taco.yaml', epochs=50, imgsz=640, freeze=10)
+    model.train(data='datasets/trash_taco.yaml', epochs=50, imgsz=640, freeze=0)
+    # model.train(data='datasets/trash_taco.yaml', epochs=50, imgsz=640, freeze=10)
 
 
